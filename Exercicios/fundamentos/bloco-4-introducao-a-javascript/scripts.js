1f2f80d92ae162d2010fe9894108bbc54af0adc4
Isabel Terenzi <bel.terenzi@gmail.com>
2022-03-07 18:29:05 -0300

    exercicios 1 2 3 4

--- Exercicios/fundamentos/bloco-4-introducao-a-javascript/scripts.js
+++ Exercicios/fundamentos/bloco-4-introducao-a-javascript/scripts.js
@@ -1,20 +1,49 @@
-let resultado = "oi";
+// exercicio 1
+let a = 2;
+let b = 6;
+console.log (a + b);
+console.log (a - b);
+console.log (a * b);
+console.log (a / b);
+console.log (a % b);
 
-switch (resultado) {
+//exercicio 2
+const c = 5;
+const d = 8;
 
-    case "aprovada":
-    console.log("Parabéns, você foi aprovada(o)!");
-    break;
-
-    case "lista":
-    console.log ("Você está na lista de espera");
-    break;
-
-    case "reprovada":
-    console.log ("Você está reprovada(o)");
-    break;
-
-    default:
-        console.log ("Não se aplica");
+if (c > d) {
+    console.log(c);
+}
+else {
+    console.log(d);
 }
 
+//exercicio 3
+const e = 9;
+const f = 20;
+const g = 30;
+
+if (e > f && e > g) {
+    console.log(e);
+}
+else if (f > e && f > g) {
+    console.log(f);
+}
+else {
+    console.log(g);
+}
+
+//exercicio 4
+const h = -10;
+
+if (h > 0) {
+    console.log("positive");
+}
+else if (h < 0) {
+    console.log("negative");
+}
+else {
+    console.log("zero");
+}
+
+//exercicio 5
