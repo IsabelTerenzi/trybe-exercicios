811ac76749a01837ec2fa66c3e5073565c85b793
Isabel Terenzi <bel.terenzi@gmail.com>
2022-03-07 18:07:40 -0300

    organiza pastas

---
+++ Exercicios/fundamentos/bloco-4-introducao-a-javascript/scripts.js
@@ -0,0 +1,20 @@
+let resultado = "oi";
+
+switch (resultado) {
+
+    case "aprovada":
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+
+    case "lista":
+    console.log ("Você está na lista de espera");
+    break;
+
+    case "reprovada":
+    console.log ("Você está reprovada(o)");
+    break;
+
+    default:
+        console.log ("Não se aplica");
+}
+
