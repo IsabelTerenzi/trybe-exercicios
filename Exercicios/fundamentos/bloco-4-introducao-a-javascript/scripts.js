2335bf7e5de35631f14bbd18f4ac81db8846a6fb
Isabel Terenzi <bel.terenzi@gmail.com>
2022-03-07 18:59:23 -0300

    exercicios 5 6 7 8

--- Exercicios/fundamentos/bloco-4-introducao-a-javascript/scripts.js
+++ Exercicios/fundamentos/bloco-4-introducao-a-javascript/scripts.js
@@ -47,3 +47,79 @@ else {
 }
 
 //exercicio 5
+const tri1 = 60;
+const tri2 = 80;
+const tri3 = 40;
+
+if (tri1 + tri2 + tri3 === 180) {
+    console.log("true");
+}
+else if (tri1 + tri2 + tri3 !== 180) {
+    console.log("false");
+}
+else {
+    console.log("invalido");
+}
+
+//exercicio 6
+let movimento = "peao";
+
+switch (movimento) {
+    case "cavalo":
+        console.log("em L");
+        break;
+
+    case "bispo":
+        console.log("diagonais");
+        break;
+
+    case "peao":
+        console.log("uma casa");
+        break;
+
+    case "torre":
+        console.log("varias casas");
+        break;
+
+    default:
+        console.log("erro");
+}
+
+//exercicio 7
+let nota = 90;
+
+if(nota >= 90) {
+    console.log("A");
+}
+else if (nota >= 80) {
+    console.log("B");
+}
+else if (nota >= 70) {
+    console.log("C");
+}
+else if (nota >=60) {
+    console.log("D");
+}
+else if (nota >=50) {
+    console.log("E");
+}
+else if (nota < 0 || nota > 100) {
+    console.log("erro");
+}
+else {
+    console.log("F");
+}
+
+//exercicio 8
+const x = 25;
+const y = 35;
+const w = 75;
+
+if (x % 2 === 0 || y % 2 === 0 || w % 2 === 0) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+
+//exercicio 9
